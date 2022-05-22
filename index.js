e8894f45f4ae220e75331a9ff5a0f3606764b91a
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-22 21:49:41 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -7,3 +8,33 @@ const port = process.env.PORT || 5000
 //use middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mwk4o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db("robotic").collection("products");
+
+// ----------get all product---------
+        
+ 
+    } 
+    finally {
+    
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!')
+  })
+   
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
