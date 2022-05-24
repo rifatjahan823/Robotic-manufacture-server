9739396f491091da520ccbc73656621ba2753f0b
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-24 10:15:37 +0600

    add jwt

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ async function run() {
     try {
       await client.connect();
       const productCollection = client.db("robotic").collection("products");
+      const orderCollection = client.db("robotic").collection("order");
+      const userCollection = client.db("robotic").collection("user");
+
+
+
+/******verify JWT********/
+function verifyJWT(req,res,next){
+  const authHeader =req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message:'authorization'})
+  }
+  const token =authHeader.split(' ')[1];
+  // verify a token symmetric
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+  if(err){
+    return res.status(403).send({message:'Forbiden access'})
+  }
+  req.decoded=decoded;
+  next();
+});
+}
 
 // ----------get all product---------
     app.get("/product",async(req,res)=>{
@@ -33,9 +54,58 @@ async function run() {
             const getOneProduct = await productCollection.findOne(query);
             res.send(getOneProduct)
         })
+  
+/******get user booking information sent backend********/
+app.post('/order',async(req,res)=>{
+  const order = req.body;
+  const result = await  orderCollection.insertOne(order );
+  res.send(result)
+})
+
+/******show per user order by email********/
+app.get('/order',verifyJWT,async(req,res)=>{
+  const userEmail = req.query.userEmail;
+  const decodedEmail = req.decoded.email;
+  if(userEmail===decodedEmail){
+    const query ={userEmail:userEmail};
+    const order =await orderCollection.find(query).toArray();
+    return res.send(order)
+  }else{
+    return res.status(403).send({message:'forbiden'})
+  }
+
+})
+/******delete order by email********/
+app.delete('/order/:email',async(req,res)=>{
+  const email = req.params.email;
+  const query = {email:email}
+  const result = await orderCollection.deleteOne(query);
+  return res.send(result);
+})
+
+/******update user********/
+
+app.put('/user/:email',async(req,res)=>{
+  const email = req.params.email;
+  const user = req.body;
+  const filter = {email:email};
+  const options = {upsert:true};
+  const updateDoc = {
+    $set:user,
+  };
+  const result = await userCollection.updateOne(filter, updateDoc, options);
+  const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET);
+  res.send({result,token:token});
+})
 
 
-    } 
+
+
+
+
+
+
+    }
     finally {
     
     }
